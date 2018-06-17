b7ade73de753fda215ed6d1e5da95a1cc881def6
Philemon Berhane <philberhane@yahoo.com>
2018-06-16 19:05:50 -0700

    package changes

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(expressValidator({
 // Connect Flash
 app.use(flash());
 
-// Global Vars
+// Global Varsgit
 app.use(function (req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
